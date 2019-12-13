8e34eb0c3e9f0012e3ec202bec7c9b820248f054
Martin Kos <martinkos007@gmail.com>
2019-12-13 21:23:41 +0100

    Login without erros showing

--- Language/PhP/Project Galerie/Galerie/LoginToRodineFoto.php
+++ Language/PhP/Project Galerie/Galerie/LoginToRodineFoto.php
@@ -12,8 +12,8 @@
 
 <body>
   <?php
-  error_reporting(E_ALL);
-ini_set('display_errors', 1);
+  // error_reporting(E_ALL);
+// ini_set('display_errors', 1);
   session_start();
   $conn =  new mysqli("jestrab.kolej.mff.cuni.cz", "martin", "Kos", "martin");
   $select;
