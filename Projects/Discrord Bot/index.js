149efac3177b4ab0f5e04d43980c6390cc3f2da2
Martin Kos <martinkos007@gmail.com>
2019-12-23 18:21:55 +0100

    Whole update + java scipt

--- Projects/Discrord Bot/index.js
+++ Projects/Discrord Bot/index.js
@@ -5,6 +5,7 @@ const fs = require("fs");
 const bash = new RegExp("^!run bash ");
 const python = new RegExp("^!run python ");
 const php = new RegExp("^!run php ");
+const js = new RegExp("^!run js ");
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -19,34 +20,40 @@ function writeToFile(params) {
   });
 }
 client.on("message", msg => {
-  currentMsg = msg;
-  msgText = "";
-  if (msg.content == "!run help") {
-    msg.reply("!run bash || !run python || !run php");
-    return;
-  }
-  if (bash.test(msg.content)) {
-    let text = msg.content;
-    if (text.endsWith("`")) {
-      text = text.substring(13, text.length - 3);
-    } else {
-      text = text.substring(9, text.length);
-    }
-    msgText += "Running :``` " + text + " ``` ";
-    writeToFile(text);
-    exec("/bin/bash execute" + text, myMethod);
-  } else {
-    if (python.test(msg.content)) {
-      let text = msg.content;
-      if (text.endsWith("`")) {
-        text = text.substring(15, text.length - 3);
-      } else {
-        text = text.substring(12, text.length);
+  if (bash.test(msg.content) || php.test(msg.content) || python.test(msg.content) || js.test(msg.content)) {
+    if (msg.member.roles.find(r => r.name === "Admin") || msg.member.roles.find(r => r.name === "Owner") || msg.member.roles.find(r => r.name === "Linux Bot User")) {
+      currentMsg = msg;
+      msgText = "";
+      //HELP
+      if (msg.content == "!run help") {
+        msg.reply("!run bash || !run python || !run php");
+        return;
       }
-      msgText += "Running with python :``` " + text + " ``` ";
-      writeToFile(text);
-      exec("python execute", myMethod);
-    } else {
+      //BASH
+      if (bash.test(msg.content)) {
+        let text = msg.content;
+        if (text.endsWith("`")) {
+          text = text.substring(13, text.length - 3);
+        } else {
+          text = text.substring(9, text.length);
+        }
+        msgText += "Running with bash:``` " + text + " ``` ";
+        writeToFile(text);
+        exec("/bin/bash ./execute", myMethod);
+      }
+      //PYTHON
+      if (python.test(msg.content)) {
+        let text = msg.content;
+        if (text.endsWith("`")) {
+          text = text.substring(15, text.length - 3);
+        } else {
+          text = text.substring(12, text.length);
+        }
+        msgText += "Running with python :``` " + text + " ``` ";
+        writeToFile(text);
+        exec("python execute", myMethod);
+      }
+      //PHP
       if (php.test(msg.content)) {
         let text = msg.content;
         if (text.endsWith("`")) {
@@ -55,9 +62,23 @@ client.on("message", msg => {
           text = text.substring(8, text.length);
         }
         msgText += "Running with php :``` " + text + " ``` ";
-        writeToFile("<?php"+text+"?>");
+        writeToFile("<?php" + text + "?>");
         exec("php -f execute", myMethod);
       }
+      //Java scirpt
+      if (js.test(msg.content)) {
+        let text = msg.content;
+        if (text.endsWith("`")) {
+          text = text.substring(12, text.length - 3);
+        } else {
+          text = text.substring(8, text.length);
+        }
+        msgText += "Running with java script :``` " + text + " ``` ";
+        writeToFile(text);
+        exec("node execute", myMethod);
+      }
+    } else {
+      msg.reply("You dont have permition to do this. Ask admin for perm.");
     }
   }
 });
@@ -74,21 +95,20 @@ function replyThis(msgText) {
 function myMethod(err, stdout, stderr) {
   if (err) {
     msgText += "Error:```" + err + "``` ";
-  } else {
-    if (stdout) {
-      msgText += "Response:```" + stdout + "``` ";
-    }
-    if (stderr) {
-      msgText += "Response:```" + stderr + "``` ";
-    }
+  }
+  if (stdout) {
+    msgText += "Response:```" + stdout + "``` ";
+  }
+  if (stderr) {
+    msgText += "Response:```" + stderr + "``` ";
   }
   replyThis(msgText);
 }
 
-fs.readFile('token', (err, data) => { 
-  if (err) throw err; 
-  let token=data.toString();
-  token=token.replace(/(\r\n|\n|\r)/gm, "");
-  console.log(token); 
+fs.readFile("token", (err, data) => {
+  if (err) throw err;
+  let token = data.toString();
+  token = token.replace(/(\r\n|\n|\r)/gm, "");
+  console.log(token);
   client.login(token);
 });
