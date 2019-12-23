4a0127265b471e2dc0ed29617dcb27bc6a470334
Martin Kos <martinkos007@gmail.com>
2019-12-23 02:43:55 +0100

    Discord bot for running bash commands

--- Projects/Discrord Bot/index.js
+++ Projects/Discrord Bot/index.js
@@ -1,14 +1,39 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-})
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!')
+const { exec } = require("child_process");
+let msgText;
+client.on("message", msg => {
+  let regex = new RegExp("^!run ");
+  if (regex.test(msg.content)) {
+    let text = msg.content;
+    if (text.endsWith("`")) {
+      text = text.substring(8, text.length - 3);
+    }else{
+      text = text.substring(5, text.length);
+    }
+    msgText = "";
+    msgText += "Running :``` " + text + " ``` ";
+    exec("cd / && " + text, (err, stdout, stderr) => {
+      if (err) {
+        //some err occurred
+        msgText += "Error:``` " + err + " ``` ";
+      } else {
+        // the *entire* stdout and stderr (buffered)
+        if (stdout) {
+          msgText += "Response:``` " + stdout + " ``` ";
+        }
+        if (stderr) {
+          msgText += "Response:``` " + stderr + " ``` ";
+        }
+      }
+      msg.reply(msgText);
+    });
   }
-})
+});
 
-client.login('NjU4NDY3MjYxNTMwNTA1MjE3.XgAMgQ.udDD_dcGt_zywi-O26QFB7b758k')
+client.login("NjU4NDY3MjYxNTMwNTA1MjE3.XgAT5g.Xd2grtkmZMEwyeJASL0rysNW2xk");
