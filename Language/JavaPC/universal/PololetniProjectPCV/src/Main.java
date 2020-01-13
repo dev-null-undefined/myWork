6c9c74c423264d66bca041846462a2c9d9559403
Martin Kos <martinkos007@gmail.com>
2020-01-13 09:22:55 +0100

    PCV

--- Language/JavaPC/universal/PololetniProjectPCV/src/Main.java
+++ Language/JavaPC/universal/PololetniProjectPCV/src/Main.java
@@ -39,7 +39,7 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		if (args.length > 2) {
-			if (args[2].equalsIgnoreCase("-debug")) {
+			if (args[1].equalsIgnoreCase("-debug")) {
 				debug = true;
 			}
 		}
