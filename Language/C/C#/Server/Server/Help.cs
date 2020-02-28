ec43b0b89be2ab5a59edf54090d14f876cc414f5
Martin Kos <martinkos007@gmail.com>
2020-02-28 08:58:47 +0100

    C# server update with working chat

--- Language/C/C#/Server/Server/Help.cs
+++ Language/C/C#/Server/Server/Help.cs
@@ -5,9 +5,9 @@ using System.Text;
 
 namespace Server
 {
-    class Help : ICommand
+    class Help
     {
-        public void takeControll(Socket socket, byte[] data)
+        public static void takeControll(Socket socket, byte[] data)
         {
             throw new NotImplementedException();
         }
