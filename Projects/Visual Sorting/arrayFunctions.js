566de1ef5c4b4bcd2e39cf9db755575e557dab54
Martin Kos <martinkos007@gmail.com>
2020-03-14 23:18:22 +0100

    Format

--- Projects/Visual Sorting/arrayFunctions.js
+++ Projects/Visual Sorting/arrayFunctions.js
@@ -1,42 +1,41 @@
 // Array Functions
 function randomizePositions(array, timesToSwap = array.length) {
-    for (let i = 0; i < array.length; i++) {
-      swap(Math.floor(Math.random() * array.length), Math.floor(Math.random() * array.length), array)
+  for (let i = 0; i < array.length; i++) {
+    swap(Math.floor(Math.random() * array.length), Math.floor(Math.random() * array.length), array);
+  }
+  return array;
+}
+function isSorted(array) {
+  const sortedArray = [...array].sort();
+  let sorted = true;
+  array.forEach((element, index) => {
+    if (element !== sortedArray[index]) {
+      sorted = false;
     }
-    return array
+  });
+  return sorted;
+}
+function swap(a, b, array) {
+  const itemA = array[a];
+  array[a] = array[b];
+  array[b] = itemA;
+}
+function move(indexA, indexB, array) {
+  return array.splice(indexB, 0, array.splice(indexA, 1)[0]);
+}
+function generateArray(length) {
+  const array = [];
+  for (let i = 0; i < length; i++) {
+    array.push(Math.random());
   }
-  function isSorted(array) {
-    const sortedArray = [...array].sort()
-    let sorted = true
-    array.forEach((element, index) => {
-      if (element !== sortedArray[index]) {
-        sorted = false
-      }
-    })
-    return sorted
-  }
-  function swap(a, b, array) {
-    const itemA = array[a]
-    array[a] = array[b]
-    array[b] = itemA
-  }
-  function move(indexA, indexB, array) {
-    return array.splice(indexB, 0, array.splice(indexA, 1)[0])
-  }
-  function generateArray(length) {
-    const array = []
-    for (let i = 0; i < length; i++) {
-      array.push(Math.random())
+  return array;
+}
+function minIndex(array) {
+  let minimumIndex = 0;
+  array.forEach((element, index) => {
+    if (element < array[minimumIndex]) {
+      minimumIndex = index;
     }
-    return array
-  }
-  function minIndex(array) {
-    let minimumIndex = 0
-    array.forEach((element, index) => {
-      if (element < array[minimumIndex]) {
-        minimumIndex = index
-      }
-    })
-    return minimumIndex
-  }
-  
+  });
+  return minimumIndex;
+}
