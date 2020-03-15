96e3747534a97f99b2e8156aa4a341cec7e73c53
ThreshMain <ThreshMain>
2020-03-15 17:01:57 +0100

    2) udpdate

--- Kantnerova/DS/selecty s datasetem/selecty.sql
+++ Kantnerova/DS/selecty s datasetem/selecty.sql
@@ -1,7 +1,7 @@
 -- 1. vypište jména zákazníků s počtem objednávek, které mají, výpis seřaďte podle abecedy
 select customers.name, count(*)
-from customers 
-inner join orders on orders.cust_id = customers.id
+from customers
+    inner join orders on orders.cust_id = customers.id
 group by customers.id,customers.name
 order by customers.name
 -- 2. vypište jméno a zemi dodavatelů, kteří dodali výrobky za celkovou cenu (amount*price)  větší než 100 dolarů
@@ -9,38 +9,46 @@ select developers.name, developers.country
 from (
 select order_details.id, sum(amount)*sum(price) as sumPrice
     from orders
-    inner join order_details on order_details.order_id=orders.id
-    inner join products on products.id = order_details.product_id
+        inner join order_details on order_details.order_id=orders.id
+        inner join products on products.id = order_details.product_id
     group by order_details.id) as x
-inner join order_details on order_details.id=x.id
-inner join products on order_details.product_id=products.id
-inner join developers on products.dev_id=developers.id
+    inner join order_details on order_details.id=x.id
+    inner join products on order_details.product_id=products.id
+    inner join developers on products.dev_id=developers.id
 group by developers.id,developers.name,developers.country
 having sum(x.sumPrice)>100;
+
+select developers.name, developers.country
+from orders
+    inner join order_details on order_details.order_id=orders.id
+    inner join products on products.id = order_details.product_id
+    inner join developers on products.dev_id=developers.id
+group by order_details.id
+having sum(amount * price)>100
 -- 3. vypište nejlevnější výrobek, vypište jeho název a cenu
 select products.name, products.price
 from products
 where products.price=(
     select min(products.price)
-    from products);
+from products);
 -- 4. vypište výrobky dodvatelů ze země 'Indonesia', vypište jmeno a zemi dodavatele a název, cenu a id výrobku
 select developers.name, developers.country, products.name, products.price, products.id
-from products 
-inner join developers on developers.id=products.dev_id
+from products
+    inner join developers on developers.id=products.dev_id
 where developers.country='Indonesia';
 -- 5. zvyšte cenu výrobků dodvatelů ze země 'Indonesia' o 20%
 update products 
 set price=price*1.2 
-from products 
-inner join developers on products.dev_id=developers.id 
+from products
+    inner join developers on products.dev_id=developers.id 
 where developers.country='Indonesia';
 -- 6. vypište názvy výrobků, které jsou od některého z dodavatelů ze zemí 'Honduras' nebo 'Indonesia'
 select products.name
-from products 
-inner join developers on developers.id=products.dev_id
+from products
+    inner join developers on developers.id=products.dev_id
 where developers.country in ('Indonesia','Honduras');
 -- 7. vypište název a cenu výrobků, které si nikdo neobjednal
-select products.name,products.price
+select products.name, products.price
 from products
 where products.id not in (select order_details.product_id
 from order_details)
