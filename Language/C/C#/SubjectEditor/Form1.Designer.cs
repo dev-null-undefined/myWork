be2f7e1f2190dfab856e49c8b6dd76aa67c22149
Martin Kos <martinkos007@gmail.com>
2020-04-30 22:42:51 +0200

    SubjectEditor Klimes

---
+++ Language/C/C#/SubjectEditor/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace SubjectEditor
+{
+    partial class SubjectEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SubjectEditor));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.DownloadButton = new System.Windows.Forms.ToolStripButton();
+            this.UploadButton = new System.Windows.Forms.ToolStripButton();
+            this.SyncButton = new System.Windows.Forms.ToolStripButton();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.ID_Label = new System.Windows.Forms.Label();
+            this.FirstName_Label = new System.Windows.Forms.Label();
+            this.LastName_Label = new System.Windows.Forms.Label();
+            this.ID_Value = new System.Windows.Forms.TextBox();
+            this.FirstName_Value = new System.Windows.Forms.TextBox();
+            this.LastName_Value = new System.Windows.Forms.TextBox();
+            this.SchoolClass_Label = new System.Windows.Forms.Label();
+            this.SchoolClass_Value = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(40, 40);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.DownloadButton,
+            this.UploadButton,
+            this.SyncButton});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(986, 47);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // DownloadButton
+            // 
+            this.DownloadButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.DownloadButton.Image = ((System.Drawing.Image)(resources.GetObject("DownloadButton.Image")));
+            this.DownloadButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.DownloadButton.Name = "DownloadButton";
+            this.DownloadButton.Size = new System.Drawing.Size(44, 44);
+            this.DownloadButton.Text = "toolStripButton1";
+            this.DownloadButton.Click += new System.EventHandler(this.DownloadButtonClick);
+            // 
+            // UploadButton
+            // 
+            this.UploadButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.UploadButton.Image = ((System.Drawing.Image)(resources.GetObject("UploadButton.Image")));
+            this.UploadButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.UploadButton.Name = "UploadButton";
+            this.UploadButton.Size = new System.Drawing.Size(44, 44);
+            this.UploadButton.Text = "toolStripButton1";
+            this.UploadButton.Click += new System.EventHandler(this.UploadButtonClick);
+            // 
+            // SyncButton
+            // 
+            this.SyncButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.SyncButton.Image = ((System.Drawing.Image)(resources.GetObject("SyncButton.Image")));
+            this.SyncButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.SyncButton.Name = "SyncButton";
+            this.SyncButton.Size = new System.Drawing.Size(44, 44);
+            this.SyncButton.Text = "toolStripButton1";
+            this.SyncButton.Click += new System.EventHandler(this.SyncButtonClick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(28, 28);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 553);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 8, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(986, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 47);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(2);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tableLayoutPanel1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Size = new System.Drawing.Size(986, 506);
+            this.splitContainer1.SplitterDistance = 101;
+            this.splitContainer1.SplitterWidth = 2;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 39F));
+            this.tableLayoutPanel1.Controls.Add(this.ID_Label, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.FirstName_Label, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.LastName_Label, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.ID_Value, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.FirstName_Value, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.LastName_Value, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.SchoolClass_Label, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.SchoolClass_Value, 1, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(986, 101);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // ID_Label
+            // 
+            this.ID_Label.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.ID_Label.AutoSize = true;
+            this.ID_Label.Location = new System.Drawing.Point(2, 5);
+            this.ID_Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.ID_Label.Name = "ID_Label";
+            this.ID_Label.Size = new System.Drawing.Size(51, 13);
+            this.ID_Label.TabIndex = 0;
+            this.ID_Label.Text = "ID:";
+            // 
+            // FirstName_Label
+            // 
+            this.FirstName_Label.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.FirstName_Label.AutoSize = true;
+            this.FirstName_Label.Location = new System.Drawing.Point(2, 29);
+            this.FirstName_Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.FirstName_Label.Name = "FirstName_Label";
+            this.FirstName_Label.Size = new System.Drawing.Size(51, 13);
+            this.FirstName_Label.TabIndex = 1;
+            this.FirstName_Label.Text = "Jméno:";
+            // 
+            // LastName_Label
+            // 
+            this.LastName_Label.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.LastName_Label.AutoSize = true;
+            this.LastName_Label.Location = new System.Drawing.Point(2, 53);
+            this.LastName_Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.LastName_Label.Name = "LastName_Label";
+            this.LastName_Label.Size = new System.Drawing.Size(51, 13);
+            this.LastName_Label.TabIndex = 2;
+            this.LastName_Label.Text = "Příjmení:";
+            // 
+            // ID_Value
+            // 
+            this.ID_Value.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.ID_Value.Location = new System.Drawing.Point(57, 2);
+            this.ID_Value.Margin = new System.Windows.Forms.Padding(2);
+            this.ID_Value.Name = "ID_Value";
+            this.ID_Value.Size = new System.Drawing.Size(888, 20);
+            this.ID_Value.TabIndex = 3;
+            // 
+            // FirstName_Value
+            // 
+            this.FirstName_Value.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.FirstName_Value.Location = new System.Drawing.Point(57, 26);
+            this.FirstName_Value.Margin = new System.Windows.Forms.Padding(2);
+            this.FirstName_Value.Name = "FirstName_Value";
+            this.FirstName_Value.Size = new System.Drawing.Size(888, 20);
+            this.FirstName_Value.TabIndex = 4;
+            // 
+            // LastName_Value
+            // 
+            this.LastName_Value.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.LastName_Value.Location = new System.Drawing.Point(57, 50);
+            this.LastName_Value.Margin = new System.Windows.Forms.Padding(2);
+            this.LastName_Value.Name = "LastName_Value";
+            this.LastName_Value.Size = new System.Drawing.Size(888, 20);
+            this.LastName_Value.TabIndex = 5;
+            // 
+            // SchoolClass_Label
+            // 
+            this.SchoolClass_Label.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.SchoolClass_Label.AutoSize = true;
+            this.SchoolClass_Label.Location = new System.Drawing.Point(0, 80);
+            this.SchoolClass_Label.Margin = new System.Windows.Forms.Padding(0);
+            this.SchoolClass_Label.Name = "SchoolClass_Label";
+            this.SchoolClass_Label.Size = new System.Drawing.Size(55, 13);
+            this.SchoolClass_Label.TabIndex = 6;
+            this.SchoolClass_Label.Text = "Třída";
+            // 
+            // SchoolClass_Value
+            // 
+            this.SchoolClass_Value.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.SchoolClass_Value.Location = new System.Drawing.Point(57, 78);
+            this.SchoolClass_Value.Margin = new System.Windows.Forms.Padding(2, 5, 2, 2);
+            this.SchoolClass_Value.Name = "SchoolClass_Value";
+            this.SchoolClass_Value.Size = new System.Drawing.Size(888, 20);
+            this.SchoolClass_Value.TabIndex = 7;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 72;
+            this.dataGridView1.RowTemplate.Height = 31;
+            this.dataGridView1.Size = new System.Drawing.Size(986, 403);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.DefaultValuesNeeded += new System.Windows.Forms.DataGridViewRowEventHandler(this.DefaultValuesNeeded);
+            this.dataGridView1.UserAddedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.dataGridView1_UserAddedRow);
+            // 
+            // SubjectEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(986, 575);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MinimumSize = new System.Drawing.Size(400, 400);
+            this.Name = "SubjectEditor";
+            this.Text = "Subject Editor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripButton SyncButton;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label ID_Label;
+        private System.Windows.Forms.Label FirstName_Label;
+        private System.Windows.Forms.Label LastName_Label;
+        private System.Windows.Forms.TextBox ID_Value;
+        private System.Windows.Forms.TextBox FirstName_Value;
+        private System.Windows.Forms.TextBox LastName_Value;
+        private System.Windows.Forms.Label SchoolClass_Label;
+        private System.Windows.Forms.TextBox SchoolClass_Value;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ToolStripButton DownloadButton;
+        private System.Windows.Forms.ToolStripButton UploadButton;
+    }
+}
+
---
+++ Language/C/C#/SubjectEditor/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SubjectEditor
+{
+    public partial class SubjectEditor : Form
+    {
+        DataTable t = new DataTable("Subject");
+        private SqlConnection con;
+        private int newIdShift=0;
+        private NameValueCollection settings = ConfigurationManager.AppSettings;
+        public SubjectEditor()
+        {
+            InitializeComponent();
+            con = new SqlConnection("Data Source="+ settings.Get("host")+","+ settings.Get("port") + "; Database="+ settings.Get("database") + "; User ID="+ settings.Get("user") + ";password="+ settings.Get("password"));
+            Download();
+            if(settings.Get("id-Read-only")=="true"|| settings.Get("id-Read-only") == "1")
+            {
+                dataGridView1.Columns["id"].ReadOnly = true;
+                ID_Value.ReadOnly = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SyncButtonClick(object sender, EventArgs e)
+        {
+            Upload();
+            Download();
+        }
+        private void Download()
+        {
+            con.Open();
+            SqlCommand c = new SqlCommand("SELECT * FROM "+settings.Get("table"), con);
+            t.Clear();
+            SqlDataAdapter a = new SqlDataAdapter(c);
+            a.Fill(t);
+            BindingSource bs = new BindingSource(t, "");
+            dataGridView1.DataSource = bs;
+            Binding bID = new Binding("Text", bs, "id");
+            bID.DataSourceUpdateMode = DataSourceUpdateMode.OnPropertyChanged;
+            Binding bFistName = new Binding("Text", bs, "first_name");
+            bFistName.DataSourceUpdateMode = DataSourceUpdateMode.OnPropertyChanged;
+            Binding bLastName = new Binding("Text", bs, "last_name");
+            bLastName.DataSourceUpdateMode = DataSourceUpdateMode.OnPropertyChanged;
+            Binding bSchoolClass = new Binding("Text", bs, "school_class");
+            bSchoolClass.DataSourceUpdateMode = DataSourceUpdateMode.OnPropertyChanged;
+            ID_Value.DataBindings.Clear();
+            ID_Value.DataBindings.Add(bID);
+            FirstName_Value.DataBindings.Clear();
+            FirstName_Value.DataBindings.Add(bFistName);
+            LastName_Value.DataBindings.Clear();
+            LastName_Value.DataBindings.Add(bLastName);
+            SchoolClass_Value.DataBindings.Clear();
+            SchoolClass_Value.DataBindings.Add(bSchoolClass);
+            con.Close();
+        }
+        private void Upload()
+        {
+            con.Open();
+            SqlCommand selectCmd = new SqlCommand("SELECT * FROM "+ settings.Get("table"), con);
+            SqlCommand updateCmd = new SqlCommand("UPDATE "+settings.Get("table") + " SET id=@id, first_name=@firstName, mid_name=@midName , last_name=@lastName,email=@email , school_class=@schoolClass, nick=@nick, suspend=@suspend WHERE id=@updateID;", con);
+            updateCmd.Parameters.Add("@firstName", SqlDbType.NVarChar, 50, "first_name");
+            updateCmd.Parameters.Add("@midName", SqlDbType.NVarChar, 50, "mid_name");
+            updateCmd.Parameters.Add("@lastName", SqlDbType.NVarChar, 50, "last_name");
+            updateCmd.Parameters.Add("@email", SqlDbType.NVarChar, 128, "email");
+            updateCmd.Parameters.Add("@schoolClass", SqlDbType.NChar, 3, "school_class");
+            updateCmd.Parameters.Add("@nick", SqlDbType.NChar, 3, "nick");
+            updateCmd.Parameters.Add("@suspend", SqlDbType.Bit, 0, "suspend");
+            updateCmd.Parameters.Add("@id", SqlDbType.Int, 0, "id");
+            updateCmd.Parameters.Add("@updateID", SqlDbType.Int, 0, "id").SourceVersion = DataRowVersion.Original;
+            SqlCommand insert = new SqlCommand("insert into "+ settings.Get("table") + " (id,first_name, mid_name, last_name, email, school_class, nick, suspend) values (@id,@first_name, @mid_name, @last_name, @email, @school_class, @nick, @suspend)", con);
+            insert.Parameters.Add("@first_name", SqlDbType.NVarChar, 50, "first_name");
+            insert.Parameters.Add("@mid_name", SqlDbType.NVarChar, 50, "mid_name");
+            insert.Parameters.Add("@last_name", SqlDbType.NVarChar, 50, "last_name");
+            insert.Parameters.Add("@email", SqlDbType.NVarChar, 128, "email");
+            insert.Parameters.Add("@school_class", SqlDbType.NChar, 3, "school_class");
+            insert.Parameters.Add("@nick", SqlDbType.NChar, 3, "nick");
+            insert.Parameters.Add("@suspend", SqlDbType.Bit, 0, "suspend");
+            insert.Parameters.Add("@id", SqlDbType.Int, 0, "id");
+            SqlCommand delete = new SqlCommand("DELETE FROM "+ settings.Get("table") + " where id=@deleteID", con);
+            delete.Parameters.Add("@deleteID", SqlDbType.Int, 0, "id").SourceVersion = DataRowVersion.Original;
+            SqlDataAdapter a = new SqlDataAdapter();
+            a.UpdateCommand = updateCmd;
+            a.InsertCommand = insert;
+            a.DeleteCommand = delete;
+            a.Update(t);
+            con.Close();
+        }
+        private void UploadButtonClick(object sender, EventArgs e)
+        {
+            Upload();
+        }
+
+        private void DownloadButtonClick(object sender, EventArgs e)
+        {
+            Download();
+        }
+
+
+        private void DefaultValuesNeeded(object sender, DataGridViewRowEventArgs e)
+        {
+            e.Row.Cells["suspend"].Value = false;
+            e.Row.Cells["first_name"].Value = "";
+            e.Row.Cells["mid_name"].Value = "";
+            e.Row.Cells["last_name"].Value = "";
+            e.Row.Cells["email"].Value = "";
+            e.Row.Cells["school_class"].Value = "";
+            e.Row.Cells["nick"].Value = "";
+        }
+
+        private void dataGridView1_UserAddedRow(object sender, DataGridViewRowEventArgs e)
+        {
+            Console.WriteLine("Hi");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SELECT max(id)+1 from "+ settings.Get("table"), con);
+            dataGridView1.Rows[e.Row.Index-1].Cells["id"].Value = Convert.ToInt32(cmd.ExecuteScalar())+ newIdShift;
+            con.Close();
+            newIdShift++;
+        }
+
+    }
+}
