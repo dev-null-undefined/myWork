fe649b08fd9cdfec70280e48ddad8f7229748eba
ThreshMain <martinkos007@gmail.com>
2021-02-11 01:42:00 +0100

    Moved to https://github.com/ThreshMain/VisualSorting

--- Language/Java Script/Visual Sorting/scripts/sort.js
+++
@@ -1,7 +0,0 @@
-class Sort {
-  constructor() {
-    // this.inpProgress = true;
-  }
-  step() {}
-  draw(cnt) {}
-}
--- Language/Java Script/Visual Sorting/todo.md
+++
@@ -1,2 +0,0 @@
-1. use sortType.value instead of inner text
-2. make class for sort
